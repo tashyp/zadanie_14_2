d74bac8fd12bd7002b305b1148a5910ad8e424e9
Przemek Saja <przemek.saja@gmail.com>
2018-07-08 21:11:14 +0200

    Fix problem with apperance jpg. files

--- script.js
+++ script.js
@@ -22,31 +22,31 @@ var movies = [
   	id: 1,
     title: 'Harry Potter',
     desc: 'Film o czarodzieju',
-    image: 'https://1.fwcdn.pl/po/05/71/30571/7529392.5.jpg'
+    img: 'images/harry.jpg'
   },
   {
   	id: 2,
     title: 'Król Lew',
     desc: 'Film o królu sawanny',
-    image: 'images/lew.jpg'
+    img: 'images/lew.jpg'
   },
   {
   	id: 3,
     title: 'Terminator 2',
     desc: 'Film cyborgu przeniesionym w przeszłość',
-    image: 'images/terminator2.jpg'
+    img: 'images/terminator2.jpg'
   },
   {
   	id: 4,
-    title: 'Promotheus',
+    title: 'Prometheus',
     desc: 'Film o ekspedycji w kosmosie',
-    image: 'images/terminator2.jpg'
+    img: 'images/prometheus.jpg'
   },
   {
   	id: 5,
     title: 'Back to the future',
     desc: 'Film podróżnikach w czasie',
-    image: 'images/terminator2.jpg'
+    img: 'images/backfuture.jpg'
   }
 ];
 
@@ -54,7 +54,7 @@ var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
       React.createElement('p', {}, movie.desc),
-      React.createElement('image', {}, movie.image)
+      React.createElement('img', {src: movie.img})
     );
 });
 
